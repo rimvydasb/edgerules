41d5bcdec3f3f58fb317f1997b32f89bce298f4e
Rimvydas Bingelis <rimvydas.bin@gmail.com>
2026-02-10 22:03:14 +0200

    working with portable testing

---
+++ tests/wasm/test-portable.mjs
@@ -0,0 +1,22 @@
+/**
+ * The following tests will read portable folder and load JSON file as subject to test.
+ * EdgeRules code is under source, tests to be executed are under tests
+ */
+describe('DecisionService Portable Format', () => {
+    before(() => {
+        wasm.init_panic_hook();
+    });
+
+    it('testing example-commerce.json', () => {
+
+    });
+
+    it('example-insurance.json', () => {
+
+    });
+
+    it('example-loan.json', () => {
+
+    });
+});
+
